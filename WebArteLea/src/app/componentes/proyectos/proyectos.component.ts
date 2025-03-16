75b505001019e351ee81207ecaff35f787985eb9
JRichard-Prado <jrichip@hotmail.com>
2025-03-16 03:44:19 +0100

    swiper ok

--- WebArteLea/src/app/componentes/proyectos/proyectos.component.ts
+++ WebArteLea/src/app/componentes/proyectos/proyectos.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
-// import{register} from 'swiper/element/bundle';
+import { Component, OnInit } from '@angular/core';
+// import Swiper JS
+import { Swiper } from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
 @Component({
   selector: 'app-proyectos',
   imports: [],
   templateUrl: './proyectos.component.html',
   styleUrl: './proyectos.component.css'
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
 
+  ngOnInit(): void {
+    let swiper: Swiper;
+    swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      speed: 3500,
+      
+      // slidesPerView: 1,
+      grabCursor: true,
+      centeredSlides: true,
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        
+      },
+      effect: 'cube',
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 30,
+        shadowScale: 1,
+      },
+      // autoplay: {
+      //   delay: 3000,
+      // },
+    });
+  }
+  
 }
